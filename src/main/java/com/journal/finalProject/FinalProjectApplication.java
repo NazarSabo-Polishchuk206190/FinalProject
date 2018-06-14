9cc9ba41ea60758a0b55e2ab51ae2269bf171931
Piotr Adryjanczyk <piotr.adryjanczyk@ericsson.com>
2018-06-14 08:53:39 +0200

    Rest example

--- src/main/java/com/journal/finalProject/FinalProjectApplication.java
+++ src/main/java/com/journal/finalProject/FinalProjectApplication.java
@@ -1,12 +1,34 @@
 package com.journal.finalProject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FinalProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinalProjectApplication.class, args);
+	private static final Logger log = LoggerFactory.getLogger(FinalProjectApplication.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(FinalProjectApplication.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
 	}
 }
